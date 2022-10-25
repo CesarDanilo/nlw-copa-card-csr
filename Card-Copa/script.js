921ddcd65e5461fd6ed80e656b677165ba7556e2
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2022-10-25 20:54:20 -0300

    Delete Card-Copa directory

--- Card-Copa/script.js
+++
@@ -1,7 +0,0 @@
-let isIgnite = true
-function changeCard(event){
-    const card = event.currentTarget
-    const bg = isIgnite ? "exploer":"ignite"
-    isIgnite = !isIgnite
-    card.style.backgroundImage = `url(./assets/bg-${bg}.svg)`
-}
